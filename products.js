9f2c22a87763de8adcc5cd051799d82d504234d4
Eddy Lau <eddylau0328@gmail.com>
2020-10-12 22:16:10 +0800

    added products.js to contain the product list

---
+++ products.js
@@ -0,0 +1,48 @@
+const products = [
+    {
+        brandName: 'OLAY',
+        chiName: '新生高效緊緻護膚霜',
+        engName: 'Regenerist Micro-sculpting Super Cream',
+        description: '第5代皇牌Super Cream以「三重膠原技術」，刺激膠原蛋白400%增生，全方位提升膠原，同時抗氧化、補濕鎖水、保持水油平衡。',
+        rating: 3.0,
+        price: 269.00,
+    },
+    {
+        brandName: 'OLAY',
+        chiName: '新生高效緊緻活膚露',
+        engName: 'Regenerist Micro-sculpting Super Essence Water',
+        description: '蘊含近90%精華成分，極致保濕，減淡細紋，重現無痕緊緻',
+        price: 219.90,
+    },
+    {
+        brandName: 'OLAY',
+        chiName: '新生高效輕盈緊緻日霜',
+        engName: 'Regenerist Micro-sculpting Super Day Cream SPF30',
+        description: '美國進口防曬日霜，促進膠原蛋白增生，日間輕盈抗UV',
+        rating: 4.0,
+        price: 239.90,
+    },
+    {
+        brandName: 'OLAY',
+        chiName: '新生肌源青春精華露',
+        engName: 'Regenerist Miracle Boost Youth Pre-Essence',
+        description: '「導入+精華」一瓶搞定，瞬透15層肌底，加強後續修護精華深層滲透',
+        rating: 5.0,
+        price: 289.90,
+    },
+    {
+        brandName: 'OLAY',
+        chiName: '新生高效緊緻精華露',
+        engName: 'Regenerist Micro-sculpting Super Serum',
+        description: '雙重氨基酸肽專利的抗老化配方，重塑彈性，減褪細紋',
+        rating: 3.0;
+        price: 239.90,
+    },
+    {
+        brandName: 'OLAY',
+        chiName: '新生深層潔面乳',
+        engName: 'Regenerist Micro-sculpting Cleanser',
+        description : '深層潔淨同時高效抗衰老，溫和代謝老化角質',
+        price : 119.90,
+    }
+];
