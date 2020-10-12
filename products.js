e7d502a1da2fc7e6d6e8d3c31182a1564cccb38a
Eddy Lau <eddylau0328@gmail.com>
2020-10-12 23:52:19 +0800

    added image src link

--- products.js
+++ products.js
@@ -1,48 +1,70 @@
 const products = [
-    {
-        brandName: 'OLAY',
-        chiName: '新生高效緊緻護膚霜',
-        engName: 'Regenerist Micro-sculpting Super Cream',
-        description: '第5代皇牌Super Cream以「三重膠原技術」，刺激膠原蛋白400%增生，全方位提升膠原，同時抗氧化、補濕鎖水、保持水油平衡。',
-        rating: 3.0,
-        price: 269.00,
-    },
-    {
-        brandName: 'OLAY',
-        chiName: '新生高效緊緻活膚露',
-        engName: 'Regenerist Micro-sculpting Super Essence Water',
-        description: '蘊含近90%精華成分，極致保濕，減淡細紋，重現無痕緊緻',
-        price: 219.90,
-    },
-    {
-        brandName: 'OLAY',
-        chiName: '新生高效輕盈緊緻日霜',
-        engName: 'Regenerist Micro-sculpting Super Day Cream SPF30',
-        description: '美國進口防曬日霜，促進膠原蛋白增生，日間輕盈抗UV',
-        rating: 4.0,
-        price: 239.90,
-    },
-    {
-        brandName: 'OLAY',
-        chiName: '新生肌源青春精華露',
-        engName: 'Regenerist Miracle Boost Youth Pre-Essence',
-        description: '「導入+精華」一瓶搞定，瞬透15層肌底，加強後續修護精華深層滲透',
-        rating: 5.0,
-        price: 289.90,
-    },
-    {
-        brandName: 'OLAY',
-        chiName: '新生高效緊緻精華露',
-        engName: 'Regenerist Micro-sculpting Super Serum',
-        description: '雙重氨基酸肽專利的抗老化配方，重塑彈性，減褪細紋',
-        rating: 3.0;
-        price: 239.90,
-    },
-    {
-        brandName: 'OLAY',
-        chiName: '新生深層潔面乳',
-        engName: 'Regenerist Micro-sculpting Cleanser',
-        description : '深層潔淨同時高效抗衰老，溫和代謝老化角質',
-        price : 119.90,
-    }
+	{
+		id: 'product-1',
+		brandName: 'OLAY',
+		chiName: '新生高效緊緻護膚霜',
+		engName: 'Regenerist Micro-sculpting Super Cream',
+		description:
+			'第5代皇牌Super Cream以「三重膠原技術」，刺激膠原蛋白400%增生，全方位提升膠原，同時抗氧化、補濕鎖水、保持水油平衡。',
+		rating: 3.0,
+		price: 269.0,
+		image:
+			'https://res.cloudinary.com/mtree/image/upload/f_auto,q_auto/Olay_HK/zh-hk/-/media/Olay_HK/Images/Products/Regenerist%20Super%20sculpting%20cream/3_1200x1200.jpg?h=202&la=zh-HK&mw=202&w=202&v=1-202009161350',
+		ratingImage:
+			'https://res.cloudinary.com/mtree/image/upload/f_auto,q_auto/Olay_HK/zh-hk/-/media/Olay_HK/Images/Common%20Icons/RatingStars-on.png?v=1-201608042014',
+	},
+	{
+		id: 'product-2',
+		brandName: 'OLAY',
+		chiName: '新生高效緊緻活膚露',
+		engName: 'Regenerist Micro-sculpting Super Essence Water',
+		description: '蘊含近90%精華成分，極致保濕，減淡細紋，重現無痕緊緻',
+		price: 219.9,
+		image:
+			'https://res.cloudinary.com/mtree/image/upload/f_auto,q_auto/Olay_HK/zh-hk/-/media/Olay_HK/Images/Products/RG/RG_220_4.jpg?h=202&la=zh-HK&mw=202&w=202&v=1-201802260630',
+	},
+	{
+		id: 'product-3',
+		brandName: 'OLAY',
+		chiName: '新生高效輕盈緊緻日霜',
+		engName: 'Regenerist Micro-sculpting Super Day Cream SPF30',
+		description: '美國進口防曬日霜，促進膠原蛋白增生，日間輕盈抗UV',
+		rating: 4.0,
+		price: 239.9,
+		image:
+			'https://res.cloudinary.com/mtree/image/upload/f_auto,q_auto/Olay_HK/zh-hk/-/media/Olay_HK/Images/Products/RG/RG_220_dc.jpg?h=202&la=zh-HK&mw=202&w=202&v=1-201706031115',
+	},
+	{
+		id: 'product-4',
+		brandName: 'OLAY',
+		chiName: '新生肌源青春精華露',
+		engName: 'Regenerist Miracle Boost Youth Pre-Essence',
+		description:
+			'「導入+精華」一瓶搞定，瞬透15層肌底，加強後續修護精華深層滲透',
+		rating: 5.0,
+		price: 289.9,
+		image:
+			'https://res.cloudinary.com/mtree/image/upload/f_auto,q_auto/Olay_HK/zh-hk/-/media/Olay_HK/Images/Products/RG/RG_220_6.jpg?h=202&la=zh-HK&mw=202&w=202&v=1-201802260630',
+	},
+	{
+		id: 'product-5',
+		brandName: 'OLAY',
+		chiName: '新生高效緊緻精華露',
+		engName: 'Regenerist Micro-sculpting Super Serum',
+		description: '雙重氨基酸肽專利的抗老化配方，重塑彈性，減褪細紋',
+		rating: 3.0,
+		price: 239.9,
+		image:
+			'https://res.cloudinary.com/mtree/image/upload/f_auto,q_auto/Olay_HK/zh-hk/-/media/Olay_HK/Images/Products/RG/RG_220_1.jpg?h=202&la=zh-HK&mw=202&w=202&v=1-201802260630',
+	},
+	{
+		id: 'product-6',
+		brandName: 'OLAY',
+		chiName: '新生深層潔面乳',
+		engName: 'Regenerist Micro-sculpting Cleanser',
+		description: '深層潔淨同時高效抗衰老，溫和代謝老化角質',
+		price: 119.9,
+		image:
+			'https://res.cloudinary.com/mtree/image/upload/f_auto,q_auto/Olay_HK/zh-hk/-/media/Olay_HK/Images/Products/RG/RG_220_fc.jpg?h=202&la=zh-HK&mw=202&w=202&v=1-201802260630',
+	},
 ];
